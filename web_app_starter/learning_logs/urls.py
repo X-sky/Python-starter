c854d18d37e36b87fd591511a7317be168474025
X-sky <ttse0103@outlook.com>
2024-01-04 21:49:45 +0800

    feat: add template views

--- web_app_starter/learning_logs/urls.py
+++ web_app_starter/learning_logs/urls.py
@@ -8,5 +8,9 @@ app_name = "learning_logs"
 
 urlpatterns = [
     # 主页
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # 显示单个主题
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
+    # 显示所有主题
+    path("topics/", views.topics, name="topics"),
 ]
