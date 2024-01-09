96986a4f53288f6dc42ada251b9caa46802a69d2
X-sky <ttse0103@outlook.com>
2024-01-09 22:48:19 +0800

    feat(Python Crash Course): add web_app authentication * add user guard of views

--- web_app_starter/learning_logs/views.py
+++ web_app_starter/learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -14,7 +15,7 @@ def index(request):
 @login_required
 def topics(request):
     """显示所有主题"""
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
 
@@ -23,6 +24,10 @@ def topics(request):
 def topic(request, topic_id="1"):
     """显示单个主题"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求的主题属于当前用户
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -38,7 +43,9 @@ def new_topic(request):
         # POST提交的数据，对数据进行处理
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect("learning_logs:topics")
 
     # 显示空表单或指出表单数据无效
@@ -50,6 +57,9 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """在特定主题中添加条目"""
     topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != "POST":
         form = EntryForm()
